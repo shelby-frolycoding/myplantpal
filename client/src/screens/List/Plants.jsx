244271455ffddb0608ab3cb01edee9daeeb61f27
Shelby /sexton <shelbysexton@Shelbys-MacBook-Air.local>
2021-02-02 00:50:34 -0500

    f

--- client/src/screens/List/Plants.jsx
+++ client/src/screens/List/Plants.jsx
@@ -3,19 +3,20 @@ import { Link } from 'react-router-dom';
 import "./Plants.css";
 
 export default function Plants(props) {
-  const { plants, handleDelete, currentUser } = props;
+  const { plants, handleDelete, user } = props;
   return (
-    <div className ="all-plants">
+    <div className="all-plants">
       <h3>my plants</h3>
       {plants.map(plant => (
         <React.Fragment key={plant.id}>
           <Link to={`/plants/${plant.id}`}><p>{plant.name}</p></Link>
-          {currentUser?.id === plant.user_id && (
+          {user?.id === plant.user_id && (
             <>
               <Link to={`/plants/${plant.id}/edit`}><button>Edit</button></Link>
               <button onClick={() => handleDelete(plant.id)}>Delete</button>
             </>
           )}
+
         </React.Fragment>
       ))}
       <br />
