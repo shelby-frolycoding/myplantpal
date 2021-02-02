ad75007a111e00c7c20cc0547b5f0e4839d88a50
Shelby /sexton <shelbysexton@Shelbys-MacBook-Air.local>
2021-02-02 13:26:49 -0500

    f

--- client/src/components/LandingComps/HeartLogo.jsx
+++ client/src/components/LandingComps/HeartLogo.jsx
@@ -3,8 +3,8 @@ import "./HeartLogo.css"
 
 export default function HeartLogo() {
   return (
-    <div className="h-logo">
+   
       <img className="h-logo" src={logo} alt="h logo" />
-    </div>
+
   );
 }
