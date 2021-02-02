b68d757284bc57f2b97f4fcce16039c88228a8fc
Shelby /sexton <shelbysexton@Shelbys-MacBook-Air.local>
2021-02-02 10:55:26 -0500

    f

---
+++ client/src/screens/Detail/Detail.jsx
@@ -0,0 +1,49 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { addPlantType } from '../services/flavors';
+import { getOnePlant } from '../services/foods';
+
+export default function PlantDetail(props) {
+  const [plantItem, setPlantItem] = useState(null);
+  const [plantTypeId, setPlantTypeId] = useState('')
+  const { id } = useParams();
+
+  useEffect(() => {
+    const fetchPlantItem = async () => {
+      const plantData = await getOnePlant(id);
+      setPlantItem(plantData);
+    }
+    fetchPlantItem();
+  }, [id])
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const plantItem = await addPlantType({ plant_type_id: plantTypeId, plant_id: id });
+    setPlantItem(plantItem);
+  }
+
+
+  const handleChange = (e) => {
+    const { value } = e.target;
+    setPlantTypeId(value);
+  }
+
+  return (
+    <div>
+      <h3>{plantItem?.name}</h3>
+      {plantItem?.plantType.map(plantType => (
+        <p key={plantType.id}>{plantType.name}</p>
+      ))}
+
+      <form onSubmit={handleSubmit}>
+        <select defaultValue='default' onChange={handleChange}>
+          <option disabled value='default'>-- Select a type --</option>
+          {props.plantType.map(plantType => (
+            <option value={plantType.id} key={plantType.id}>{plantType.name}</option>
+          ))}
+        </select>
+        <button>add</button>
+      </form>
+    </div>
+  )
+}
