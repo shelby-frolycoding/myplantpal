5fa088148b9fdfa7c4221190bee939c9881a2bbc
Shelby /sexton <shelbysexton@Shelbys-MacBook-Air.local>
2021-02-02 14:43:49 -0500

    f

--- client/src/screens/List/Plants.jsx
+++ client/src/screens/List/Plants.jsx
@@ -6,22 +6,29 @@ import {
   faTrashAlt,
   faPencilAlt,
   faPlusCircle,
+  faSeedling,
+  faTint,
 } from "@fortawesome/free-solid-svg-icons";
 export default function Plants(props) {
   const { plants, handleDelete, user } = props;
   console.log(plants)
   return (
     <div className="all-plants">
-      <h3>my plants</h3>
-       
+      <h3 className= "header-plant">my plants</h3>
+       <div className="plant-card">
       {plants.map(plant => (
         <React.Fragment key={plant.id}>
-          <Link className="plant-link" to={`/plants/${plant.id}`}><p className= "plant-name">{plant.name}</p></Link>
+          <Link className="plant-link" to={`/plants/${plant.id}`}>
+            <div className="plant-name">{plant.name}
+              <FontAwesomeIcon className="leaf" icon={faSeedling} />
+              <FontAwesomeIcon className= "drop" icon={faTint} />
+            </div>
+          </Link>
           
           {user?.id === plant.user_id && (
             <>
       
-              <Link className="plant-link" to={`/plants/${plant.id}/edit`}> <button className="edit-button">
+              <Link className="plant-link" to={`/plants/${plant.id}/edit`}> <button className="edit-butt">
                   <FontAwesomeIcon icon={faPencilAlt} />
                 </button></Link>
               <button className= "bye-butt" onClick={() => handleDelete(plant.id)}><FontAwesomeIcon icon={faTrashAlt} /></button>
@@ -30,6 +37,7 @@ export default function Plants(props) {
 
         </React.Fragment>
       ))}
+        </div>
       <br />
       <Link className="plant-link" to='/plants/new'><button className="add-butt"> <FontAwesomeIcon icon={faPlusCircle} /></button></Link>
     </div>
