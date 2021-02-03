89845052269feec085ca8c426b37a5054ac71178
Shelby /sexton <shelbysexton@Shelbys-MacBook-Air.local>
2021-02-03 09:29:51 -0500

    f

--- client/src/services/api-config.js
+++ client/src/services/api-config.js
@@ -1,7 +1,7 @@
 
 import axios from 'axios';
 
-const baseUrl = process.env.NODE_ENV === 'production' ? 'https://git.heroku.com/plant-back-end88.git' : 'http://localhost:3000' 
+const baseUrl = process.env.NODE_ENV === 'production' ? 'https://plant-back-end88.herokuapp.com/' : 'http://localhost:3000' 
 
 const api = axios.create({
   baseURL: baseUrl
